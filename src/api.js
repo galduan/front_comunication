7c7fa8a66234e203e337493b57faa106b7c4bcb3
gal_duan <galduan99@gmail.com>
2023-01-13 00:07:37 +0200

    x

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = 'https://comunication.herokuapp.com/';
-const API_URL = "http://127.0.0.1:8000/";
+const API_URL = 'https://comunication.herokuapp.com/';
+// const API_URL = "http://127.0.0.1:8000/";
 
 // Register user
 export const regiserUser = async (
